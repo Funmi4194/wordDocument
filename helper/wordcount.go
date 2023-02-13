b1a83d394462a71baff46d7978a7d4b1e460cd63
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-02-13 10:35:28 +0100

    feat-user-document

--- helper/wordcount.go
+++ helper/wordcount.go
@@ -2,6 +2,7 @@ package helper
 
 import "strings"
 
+//Counts the numbers of words
 func Content(content string) int {
 	word := strings.Join(strings.Fields(content), " ")
 	count := len(strings.Split(word, " "))
@@ -9,12 +10,14 @@ func Content(content string) int {
 	return count
 }
 
+//Counts the numbers of charcters
 func TotalCharacters(content string) int {
 	count := len(strings.Split(content, ""))
 
 	return count
 }
 
+//Counts the numbers of charcaters excluding spaces
 func CharWithoutSpace(content string) int {
 
 	content2 := strings.Replace(content, " ", "", -1)
@@ -23,18 +26,21 @@ func CharWithoutSpace(content string) int {
 	return count
 }
 
+//Counts the numbers of sentences
 func Sentence(content string) int {
 	count := len(strings.Split(content, "."))
 
 	return count
 }
 
+//counts the numbers of paragraphs
 func Paragraphs(content string) int {
 	count := len(strings.Split(content, "\n\n"))
 
 	return count
 }
 
+//Counts the numbers of lines
 func Line(content string) int {
 	lines := strings.SplitAfterN(content, "\n", -1)
 	count := len(lines)
